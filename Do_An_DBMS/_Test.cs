6f577868ae6b4a71cd5be323463a2b8fa7ae93ab
HQuan1428 <haiquan@LAPTOP-IJM5QA95>
2024-10-24 14:39:17 +0700

    Test

---
+++ Do_An_DBMS/_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An_DBMS
+{
+    internal class _Test
+    {
+        public void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world\n");
+        }
+    }
+}
