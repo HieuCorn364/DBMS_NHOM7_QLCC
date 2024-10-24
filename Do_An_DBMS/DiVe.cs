5c69b9699b0f085e04fb58b45a5f5cb8049ccf4f
HieuCorn364 <hieungo.030604@gmail.com>
2024-10-24 15:58:56 +0700

    DiVe

---
+++ Do_An_DBMS/DiVe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An_DBMS
+{
+    internal class DiVe
+    {
+        System.Data.SqlClient.SqlParameter[] _parameters;
+    }
+}
